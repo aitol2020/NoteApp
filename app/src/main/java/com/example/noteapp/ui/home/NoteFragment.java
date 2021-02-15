401c1c2c4419fdbb26ed01349182e18df63791d0
aitol2020 <ajtolasyrakunov>
2021-02-15 19:20:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/noteapp/ui/home/NoteFragment.java
@@ -0,0 +1,23 @@
+package com.example.noteapp.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.noteapp.R;
+
+
+public class NoteFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+
+    }
+}
